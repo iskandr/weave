ea97257aaa766dcef7edf0039ee1c7def2327ebc
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2021-12-17 15:27:14 -0500

    added 001

---
+++ misc.js
@@ -0,0 +1,45 @@
+function arange(n) {
+    // generate an array with entries 0 .. n-1
+    a = new Array(n)
+    for (let i = 0; i < n; ++i) {
+        a[i] = i;
+    }
+    return a;
+}
+
+
+function rotate(a, n) {
+    // rotate array contents by n positions
+    new_a = new Array(a.length)
+    for (let i = 0; i < a.length; ++i) {
+        let j = i + n % a.length
+        new_a[j] = a[i];
+    }
+    return new_a;
+}
+
+function reverse(a) {
+    let new_a = new Array(a.length)
+    for (let i = 0; i < a.length; ++i) {
+        new_a[i] = a[a.length - (i  + 1)];
+    }
+    return new_a
+}
+
+function concat(a, b) {
+    //concatenate two arrays
+    let n = a.length + b.length;
+    let new_a = new Array(n)
+    for (let i = 0; i < a.length; ++i) {
+        new_a[i] = a[i];
+    }
+    for (let i = 0; i < b.length; ++i) {
+        new_a[i + a.length] = b[i];
+    }
+    return new_a;
+}
+
+function symmetrize(a) {
+    // make series symmetric by concatenating it with its inverse
+    return concat(a, reverse(a));
+}
---
+++ rand.js
@@ -0,0 +1,32 @@
+const rand = Math.random
+
+function randInt(min, max) {
+    let range = max - min;
+    return min + Math.floor(Math.random() * range);
+}
+
+
+function randBool(p) {
+    return rand() > p
+}
+
+function randFloat(min, max) {
+    let range = max - min;
+    return min + (Math.random() * range);
+}
+function randRound(x) {
+    let min = Math.floor(x)
+    let max = Math.ceil(x)
+    let p = x - min
+    if (randBool(p)) {
+        return max;
+    } else {
+        return min;
+    }
+
+}
+
+
+function random(choices) {
+    return choices[Math.min(choices.length - 1, randInt(0, choices.length))];
+}
