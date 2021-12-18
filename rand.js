5591369a7fc48dd75e325b83c4c2466d4ab95415
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2021-12-18 18:27:32 -0500

    added slider versions

--- rand.js
+++ rand.js
@@ -1,19 +1,37 @@
-const rand = Math.random
+var seed = 6;
+
+function setSeed(x) {
+    seed = x;
+}
+
+function randFloat(min, max) {
+    max = max || 1;
+    min = min || 0;
+
+    seed = (seed * 9301 + 49297) % 233280;
+    var rnd = seed / 233280;
+    return min + rnd * (max - min);
+}
+function random() {
+    return randFloat(0.0, 1.0);
+}
 
 function randInt(min, max) {
     let range = max - min;
-    return min + Math.floor(Math.random() * range);
+    return min + Math.floor(random() * range);
 }
 
 
 function randBool(p=0.5) {
-    return rand() > p
+    return randFloat() > p
 }
-
+/*
 function randFloat(min, max) {
     let range = max - min;
     return min + (Math.random() * range);
 }
+ */
+
 function randRound(x) {
     let min = Math.floor(x)
     let max = Math.ceil(x)
@@ -27,6 +45,6 @@ function randRound(x) {
 }
 
 
-function random(choices) {
+function choose(choices) {
     return choices[Math.min(choices.length - 1, randInt(0, choices.length))];
 }
